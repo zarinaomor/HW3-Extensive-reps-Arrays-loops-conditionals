1dbda275ae21d4d24e2a0410e0c0cb87ec921590
zarinaomor <zarinaomorova@yahoo.com>
2019-03-20 17:00:29 -0700

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -10,5 +10,11 @@ for (let i=0; i<=200; i+=2) {
 
 // Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 for (let i=1; i<=20; i++) {
-    console.log("Love me, pet me! HSSSSSS!");
+    // console.log("Love me, pet me! HSSSSSS!");
+}
+// For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+var array = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away...", "...why does the red dot always get away..."];
+for (let i=1; i<=20; i++) {
+    let newArray = Math.floor(Math.random()) * array.length;
+     console.log(newArray);
 }
