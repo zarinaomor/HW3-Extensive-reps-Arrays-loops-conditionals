a36c91ed22b4bcf60b217b29fcd0a22dd53b15c6
zarinaomor <zarinaomorova@yahoo.com>
2019-03-20 17:01:49 -0700

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -16,5 +16,25 @@ for (let i=1; i<=20; i++) {
 var array = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away...", "...why does the red dot always get away..."];
 for (let i=1; i<=20; i++) {
     let newArray = Math.floor(Math.random()) * array.length;
-     console.log(newArray);
+    //  console.log(newArray);
 }
+
+// Fizz Buzz
+
+// Write a javascript application that logs all numbers from 1 - 100
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+for (let i=1; i<=100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if(i % 3 === 0) {
+        console.log("Fizz");
+    } else if(i % 5 === 0) {
+        console.log("Buzz");
+    }else {
+        // console.log(i);
+    }
+}
+
